6379b683d0a00e001d2398c06b1572be09fa6dc1
Patricio Navarrete <patricionavarrete.c01@gmail.com>
2020-02-13 11:47:28 -0400

    primer commit

---
+++ configApp.php
@@ -0,0 +1,29 @@
+<?php
+
+/* Go to My Apps dashboard: https://developers.mercadolibre.com.ar/apps/home, and get the information you need in order to the following enviroment variables */
+
+/* Your Application Id */
+$appId = getenv('2798433908353163');
+
+/* Your Secret Key */
+$secretKey = getenv('A69EfRLedHzUh342yWJm4vQSbPSNMlsQ');
+
+/* The Redirect url */
+$redirectURI = getenv('https://autosse.herokuapp.com');
+
+/* The site id of the country where your application will work.
+If you don't know your site_id go to our sites resources: https://api.mercadolibre.com/sites  */
+$siteId = 'MLC';
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//If you don't use Heroku use the next config
+
+// $appId = 'App_ID';
+
+// $secretKey = 'Secret_Key';
+
+// $redirectURI = 'Redirect_URI';
+
+// $siteId = 'MLB';
